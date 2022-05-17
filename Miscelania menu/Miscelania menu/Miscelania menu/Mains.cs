fe67a0cd05cd80e2dcd85e2781a0abd13cfe665f
Lauracastiblanco <104031757+Lauracastiblanco@users.noreply.github.com>
2022-05-17 17:46:50 -0500

    Add files via upload

---
+++ Miscelania menu/Miscelania menu/Miscelania menu/Mains.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscelania_menu
+{
+    internal class Mains
+
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Menu menu = new Menu();
+                menu.menu();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("-----[Ingresa un numero valido]-----");
+            }
+        }
+    }
+}
+
